58a03dcf6d8afd1ac2d676a4f73b8c81787f714b
Musashi Sakamoto <1292602b@gmail.com>
2020-05-06 10:59:43 -0700

    loginFormのUI作成

---
+++ src/components/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import LoginForm from './LoginForm';
+
+export default {
+  title: 'LoginForm',
+  component: 'LoginForm',
+};
+
+export const index = () => <LoginForm />;
