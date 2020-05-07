7b1c88a8c330a2b5bfb9893dd61dae5171221e87
Musashi Sakamoto <1292602b@gmail.com>
2020-05-06 22:55:42 -0700

    mongoose疎通確認

--- src/middlewares/db.ts
+++ src/middlewares/db.ts
@@ -1,15 +1,14 @@
 import Mongoose from 'mongoose';
 import { NextApiHandler, NextApiRequest, NextApiResponse } from 'next';
 
-const DB_URL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/portfolio-app';
+const DB_URL = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/game-of-thrones';
 
 export const dbConnected = (fn: NextApiHandler) => async (
   req: NextApiRequest,
   res: NextApiResponse,
 ) => {
-  let connection = null;
   try {
-    connection = await Mongoose.createConnection(DB_URL, {
+    await Mongoose.connect(DB_URL, {
       useNewUrlParser: true,
       bufferCommands: false,
       bufferMaxEntries: 0,
@@ -17,7 +16,6 @@ export const dbConnected = (fn: NextApiHandler) => async (
     });
     await fn(req, res);
   } catch (error) {
-    connection?.close();
     res.status(500).json({ error: 'DB Connection Error' });
   }
 };
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,14 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { sampleUserData } from '../../../utils/sample-data';
 import { dbConnected } from '../../../middlewares/db';
+import Character from '../../../models/Character';
 
 export default dbConnected(async (_: NextApiRequest, res: NextApiResponse) => {
   try {
-    if (!Array.isArray(sampleUserData)) {
-      throw new Error('Cannot find user data');
-    }
+    const characters = await Character.find();
 
-    res.status(200).json(sampleUserData);
+    res.status(200).json(characters);
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message });
   }
