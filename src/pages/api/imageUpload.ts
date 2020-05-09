83e92e6b29f7a5b2967d596462c1fd93d6b4ca16
Musashi Sakamoto <1292602b@gmail.com>
2020-05-09 09:33:05 -0700

    upload後のimage削除

--- src/pages/api/imageUpload.ts
+++ src/pages/api/imageUpload.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { v2 as cloudinary } from 'cloudinary';
 import formidable from 'formidable';
+import { promises as fs } from 'fs';
 
 cloudinary.config({
   cloud_name: 'dmrtlwisi',
@@ -14,17 +15,28 @@ export const config = {
   },
 };
 
+const formidablePromise = (req: NextApiRequest) => {
+  return new Promise<{ fields: formidable.Fields, files: formidable.Files }>((resolve, reject) => {
+    const form = new formidable.IncomingForm();
+    form.uploadDir = './';
+    form.keepExtensions = true;
+    form.parse(req, async (err, fields, files) => {
+      if (err) return reject(err);
+      resolve({ fields, files });
+    });
+  });
+};
+
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
     try {
       const form = new formidable.IncomingForm();
       form.uploadDir = './';
       form.keepExtensions = true;
-      form.parse(req, async (err, fields, files) => {
-        console.log(err, fields, files);
-        const result = await cloudinary.uploader.upload(files.image.path);
-        res.json({ result, message: 'Hey! This is your server response!' });
-      });
+      const { files } = await formidablePromise(req);
+      const result = await cloudinary.uploader.upload(files.image.path);
+      fs.unlink(files.image.path);
+      res.json({ result, message: 'Hey! This is your server response!' });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: 'Failed to upload image' });
